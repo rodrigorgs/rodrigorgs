a2d7d674f60f9a938bd27b285a2832ded49733fe
rodrigorgs <rodrigorgs@dd92322b-a053-0410-994d-05c5753ab110>
2008-09-04 21:12:45 +0000

    Adicionando doclet que imprime documentacao de classes

--- rconformance/trunk/lib/ir_distance.rb
+++ rconformance/trunk/lib/ir_distance.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+
 require 'set'
 
 class DocSpace
@@ -8,12 +10,6 @@ class DocSpace
 	end
 	
 	def add_doc(doc)
-		@doc_list << doc
-		return doc
-	end
-
-	def add_text(text)
-		doc = Document.new text
 		@doc_list << doc
 		return doc
 	end
@@ -59,18 +55,20 @@ class DocSpace
 			den1 += @weight[[term,doc]]**2
 			den2 += @weight[[term,query]]**2
 		end
-		return num.fdiv (Math.sqrt(den1) * Math.sqrt(den2))
+		return num.fdiv(Math.sqrt(den1) * Math.sqrt(den2))
 	end
 end
 
 class Document
 	#attr_reader :freq_rel
+	attr_reader :name, :text
 
 	def freq_rel(term)
 		@freq_rel[term] || 0
 	end
 
-	def initialize(text)
+	def initialize(name, text)
+		@name = name
 		@text = text
 		@freq_abs = Document::compute_term_freq(text)
 	
@@ -96,42 +94,34 @@ class Document
 	end
 end
 
+DELIMITER = '---IRDelimiter---'
 
-space = DocSpace.new
+def print_dists(instream, outstream)
+	space = DocSpace.new
+	# TODO: catch EOF exception
+	line = ''
+	line = instream.readline while line.strip != DELIMITER
 
-a = space.add_text <<EOL
-This is the JMX interface for the runtime statistics for the data node. Many of the statistics are sampled and averaged on an interval which can be specified in the config file.
+	# TODO: catch empty document (input consists of two delimiters)
+	while !instream.eof?
+		name = instream.readline.strip
+		text = ''
+		begin 
+			line = instream.readline
+			text += line if line.strip != DELIMITER
+		end while line.strip != DELIMITER
+		space.add_doc(Document.new name, text)
+	end
 
-For the statistics that are sampled and averaged, one must specify a metrics context that does periodic update calls. Most do. The default Null metrics context however does NOT. So if you aren't using any other metrics context then you can turn on the viewing and averaging of sampled metrics by specifying the following two lines in the hadoop-meterics.properties file:
+	space.done
 
-        dfs.class=org.apache.hadoop.metrics.spi.NullContextWithUpdateThread
-        dfs.period=10
-  
+	docs = space.doc_list
+	n = docs.size
+	(0..n-1).each do |i|
+		(i..n-1).each do |j|
+			outstream.puts "#{docs[i].name} #{docs[j].name} #{space.dist(docs[i], docs[j])}"
+		end
+	end
+end
 
-Note that the metrics are collected regardless of the context used. The context with the update thread is used to average the data periodically.
-
-Name Node Status info is reported in another MBean 
-EOL
-
-b = space.add_text <<EOL
-This Interface defines the methods to get the status of a the FSDataset of a data node. It is also used for publishing via JMX (hence we follow the JMX naming convention.)
-
-Data Node runtime statistic info is report in another MBean 
-EOL
-
-c = space.add_text <<EOL
-This class is for maintaining the various DataNode statistics and publishing them through the metrics interfaces. This also registers the JMX MBean for RPC.
-
-This class has a number of metrics variables that are publicly accessible; these variables (objects) have methods to update their values; for example:
-
-blocksRead.inc() 
-EOL
-
-space.done
-
-p space.terms
-p space.idf
-
-puts space.dist a, b
-puts space.dist a, c
-puts space.dist b, c
+print_dists STDIN, STDOUT
