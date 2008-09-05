1f37729d571061635759fc2a52047574bc32863b
rodrigorgs <rodrigorgs@dd92322b-a053-0410-994d-05c5753ab110>
2008-09-05 12:54:59 +0000

    Correcoes em MQFuzzyRanker.

--- trunk/rconformance/trunk/lib/MQFuzzyRanker.rb
+++ trunk/rconformance/trunk/lib/MQFuzzyRanker.rb
@@ -3,24 +3,20 @@ require 'java'
 
 # TODO: cleanup imports
 import 'edu.uci.ics.jung.graph.Graph'
-import 'edu.uci.ics.jung.graph.Vertex'
-import 'edu.uci.ics.jung.graph.impl.SparseVertex'
-import 'edu.uci.ics.jung.graph.impl.SparseGraph'
-import 'edu.uci.ics.jung.graph.impl.DirectedSparseEdge'
-import 'edu.uci.ics.jung.graph.impl.UndirectedSparseEdge'
-import 'edu.uci.ics.jung.algorithms.blockmodel.GraphCollapser' 
+#import 'edu.uci.ics.jung.graph.Vertex'
+#import 'edu.uci.ics.jung.graph.impl.SparseVertex'
+#import 'edu.uci.ics.jung.graph.impl.SparseGraph'
+#import 'edu.uci.ics.jung.graph.impl.DirectedSparseEdge'
+#import 'edu.uci.ics.jung.graph.impl.UndirectedSparseEdge'
+#import 'edu.uci.ics.jung.algorithms.blockmodel.GraphCollapser' 
 
-import 'abstractor.cluster.mq.optimization.MQTurboRanker'
 import 'abstractor.cluster.mq.optimization.MQRanker'
-import 'abstractor.cluster.jung.VertexClusterSet'
-import 'abstractor.cluster.GeneralClusterer'
-import 'abstractor.cluster.ContainerClusterer'
-import 'abstractor.cluster.RegexClusterer'
+import 'abstractor.cluster.mq.MQClusterer'
 import 'abstractor.util.FileUtilities'
 
 import 'design.model.Design'
-import 'design.model.HierarchicalSparseGraph'
-import 'design.util.Utilities'
+#import 'design.model.HierarchicalSparseGraph'
+#import 'design.util.Utilities'
 
 class MQFuzzyRanker
 	include MQRanker
@@ -63,21 +59,48 @@ class MQFuzzyRanker
 			sumE += sumEpsilon / (2 * cluster1.size * cluster2.size)
 		end
 		k = clusterSet.size
-		return somatorio_e / ((k * (k - 1)) / 2.0)
+		return sumE / ((k * (k - 1)) / 2.0)
 	end
 
 	def calculateMQ(clusterSet)
 		intra_connectivity = calculate_cohesion(clusterSet)
 		inter_connectivity = calculate_inter(clusterSet)
 
-		return inter_connectivity - intra_connectivity
+		return intra_connectivity - inter_connectivity
 	end
 end
 
 class Distance
+	def initialize
+		@dists = Hash.new
+	end
+
 	def distance(e1, e2)
-		0.5
+		d = @dists[[e1,e2]]
+		if d.nil?
+			d = rand
+			@dists[[e1,e2]] = d
+			return d
+		else
+			return d
+		end
+		#0.5 # rand
 	end
 end
+
+def abstract(inputFilename, outputFilename, propFilename)
+	d = Design.new inputFilename
+
+	props = FileUtilities.loadProperties propFilename
+
+	graph = d.getGraph(d.size - 1)
+	mqClusterer = MQClusterer.new(graph, props);
+	mqClusterer.setRanker(MQFuzzyRanker.new(Distance.new))
+	d.addGraph(mqClusterer.getClusteredDesign);
+
+	d.saveDesign outputFilename
+end
+
+abstract ARGV[0], ARGV[1], 'mqcluster.properties'
 
 # ranker = MQFuzzyRanker.new(Distance.new)
