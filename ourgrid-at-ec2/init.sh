6abfafeec050d4f6615c81b9a775d1b08359f467
rodrigorgs <rodrigorgs@dd92322b-a053-0410-994d-05c5753ab110>
2008-09-11 20:40:31 +0000

    Atualizando arquivo de inicializacao do ourgrid no ec2.

--- ourgrid-at-ec2/init.sh
+++ ourgrid-at-ec2/init.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
+# This script configures a OurGrid Worker on a pre-configured AMI (Amazon
+# Machine Image)
 
-PEER_FILE=peer-3.3.2.tar.gz
-UA_FILE=useragent-3.3.1.tar.gz
-PEER_URL=http://www.ourgrid.org/twiki-public/pub/OG/OurDownload/$PEER_FILE
-UA_URL=http://www.ourgrid.org/twiki-public/pub/OG/OurDownload/$UA_FILE
+# This section must be configured before this script can be used
+# Please surround the values with single quotes (') and precede each slash (/)
+# with a backslash (\).
 
-sed -i /etc/resolv.conf -e 's/nameserver.*/nameserver 208.67.222.222/'
-wget $UA_URL
-tar xzf $UA_FILE
+USER=USERNAME
+PASS=PASSWORD
+KEY=PEER_PUBLIC_KEY
+SERVER=xmpp.ourgrid.org
 
-#if [ $with_peer ]; then
-#	wget $PEER_URL
-#	tar xzf $PEER_FILE
-#
-#	cd peer
-#	bin/peer start
-#	#bin/peer setgums ...
-#	cd ..
-#fi
+###################################################
 
+KEY_SERVER=commune.xmpp.servername
+KEY_USER=commune.xmpp.username
+KEY_PWD=commune.xmpp.password
+KEY_PEER_PK=worker.peer.publickey
+WORKER_DIR=/root/worker-4.0alpha3
+
+cd $WORKER_DIR
+
+sed -i worker.properties -e "s/$KEY_SERVER\\=.*$/$KEY_SERVER=$SERVER/"
+sed -i worker.properties -e "s/$KEY_PEER_PK\\=.*$/$KEY_PEER_PK=$KEY/"
+sed -i worker.properties -e "s/$KEY_USER\\=.*$/$KEY_USER=$USER/"
+sed -i worker.properties -e "s/$KEY_PWD\\=.*$/$KEY_PWD=$PASS/"
+
+sh worker start
 
