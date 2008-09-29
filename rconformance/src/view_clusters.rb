5aa190ba925d331ab8c0b27bdbcf1718150d9e81
rodrigorgs <rodrigorgs@dd92322b-a053-0410-994d-05c5753ab110>
2008-09-29 11:24:22 +0000

    Adicionando visualizador simples de clusters.

---
+++ rconformance/src/view_clusters.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env jruby
+
+require 'tempfile'
+
+# clusters is a list of lists
+def view_clusters(clusters, &block)
+	tmp = Tempfile.new('viewc')
+	filename = tmp.path
+	tmp.close!
+
+	block = lambda{|x| x} if block.nil?
+
+	File.open(filename, 'w') do |file|
+		file.puts "<html><body>"
+		file.puts "<ul>"
+		clusters.each_with_index do |cluster, i|
+			file.puts "<li>Cluster #{i}</li>"
+			file.puts "<ul>"
+			cluster.each { |entity| file.puts "<li>#{block.call(entity)}</li>" }
+			file.puts "</ul>"
+		end
+		puts "</ul>"
+		file.puts "</body></html>"
+	end
+
+	`firefox #{filename}`
+end
+
+# view_clusters([[:banana, :maca], [:aviao, :carro]]) { |e| e.to_s.upcase }
+
