5a07d8e78c855a3876372a611f0727c1fb31b101
rodrigorgs <rodrigorgs@dd92322b-a053-0410-994d-05c5753ab110>
2008-09-29 11:07:27 +0000

    Melhorando visualizacao de tabela: incluindo labels e criando funcoes auxiliares para gerar a tabela de similaridades.

--- trunk/rconformance/src/evolucao2008/view_similarity.rb
+++ trunk/rconformance/src/evolucao2008/view_similarity.rb
@@ -8,7 +8,6 @@ import 'design.model.Design'
 import 'abstractor.cluster.hierarchical.DerivedAgglomerativeClusterer'
 import 'abstractor.cluster.hierarchical.CompleteLinkage'
 import 'abstractor.cluster.hierarchical.CharPairSimilarity'
-import 'abstractor.cluster.hierarchical.SimilarityPower'
 
 #sim = CompleteLinkage.new(CharPairSimilarity.new)
 sim = CharPairSimilarity.new
@@ -17,18 +16,21 @@ sim = CharPairSimilarity.new
 base = '/home/rodrigo/experimentos/systems'
 clusterer = 'abstractor.cluster.hierarchical.DerivedAgglomerativeClusterer'
 
-design = Design.new '/home/rodrigo/experimentos/systems/l1/06_SweetHome3D_l1.gxl'
+design = Design.new '/home/rodrigo/experimentos/systems/l1/01_junit_l1.gxl'
 
 vertices = design.getGraph.getVertices.toArray
 n = vertices.size
 
-array2d = Array.new(n) { Array.new(n) { 1.0 } }
+matrix = fn_to_matrix(n) { |i, j| sim.similarity(vertices[i], vertices[j]) }
+matrix_view(matrix, vertices.map{ |v| v.getUserDatum("label") })
 
-0.upto(n-1) do |i|
-	i.upto(n-1) do |j|
-		array2d[i][j] = array2d[j][i] = sim.similarity(vertices[i], vertices[j])
-	end
-end
-
-array2d_to_html(array2d)
-array2d_to_html(array2d.map{|row| row.map{|e| e**4}})
+#array2d = Array.new(n) { Array.new(n) { 1.0 } }
+#
+#0.upto(n-1) do |i|
+#	i.upto(n-1) do |j|
+#		array2d[i][j] = array2d[j][i] = sim.similarity(vertices[i], vertices[j])
+#	end
+#end
+#
+#array2d_to_html(array2d)
+#array2d_to_html(array2d.map{|row| row.map{|e| e**4}})
--- trunk/rconformance/src/view_table.rb
+++ trunk/rconformance/src/view_table.rb
@@ -1,16 +1,35 @@
 require 'tempfile'
+require 'matrix'
+
+def fn_to_array(n, &block)
+	array = Array.new(n) { Array.new(n) { 1.0 } }
+	0.upto(n - 1) do |i|
+		(i + 1).upto(n - 1) do |j|
+			array[i][j] = array[j][i] = block.call(i, j)
+		end
+	end
+	return array
+end
+
+def fn_to_matrix(n, &block)
+	array = fn_to_array(n, &block)
+	return Matrix[*array]
+end
 
 # matrix is an array of arrays
-def array2d_to_html(array2d)
-	tmp = Tempfile.new('array2d')
+def matrix_view(matrix, labels)
+	tmp = Tempfile.new('mview')
 	path = tmp.path
 	tmp.close!
 
 	File.open(path, 'w') do |file|
 		file.puts '<table border="0" cellpadding="0" cellspacing="0">'
-		array2d.each do |row|
-			file.puts '<tr>'
-			row.each do |n| 
+		file.puts "<tr><td></td>"
+		0.upto(matrix.row_size-1) { |i| file.puts "<td align=\"center\">#{i}</td>" }
+		file.puts "</tr>"
+		0.upto(matrix.row_size-1) do |i|
+			file.puts "<tr><td><tt><b>#{'%3i' % i}</b></tt>&nbsp;#{labels[i]}</td>"
+			matrix.row(i) do |n|
 				color = "#00#{"%02X" % (255*n).to_i}00"
 				file.puts "<td bgcolor=\"#{color}\">#{'%.2f' % n}</td>" 
 			end
