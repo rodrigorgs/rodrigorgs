157f401141932703c3d98303f17c603425dc63da
rodrigorgs <rodrigorgs@dd92322b-a053-0410-994d-05c5753ab110>
2008-11-05 01:51:01 +0000

    Implementando pequena linguagem para pipe de funcoes ruby.

--- trunk/softsim/plot_clustering_coefficient.rb
+++ trunk/softsim/plot_clustering_coefficient.rb
@@ -13,14 +13,8 @@ def compute_node_degrees(pairs)
   return hash
 end
 
-
-def entities(pairs)
-  pairs.flatten.uniq
-end
-
-require 'set'
 # undirected
-def compute_clustering_coefficients(pairs)
+def node_clustering_coefficients(pairs)
   STDERR.puts pairs.size
   # remove duplicate pairs -- note that [a, b] == [b, a]
   pairs = pairs.map{ |pair| pair.sort }.uniq
@@ -39,39 +33,6 @@ def compute_clustering_coefficients(pairs)
   return hash
 end
 
-filename = ARGV[0]
-relations = ARGV[1..-1]
-pairs = read_rsf_pairs(filename, relations)
-hash = compute_clustering_coefficients(pairs)
-#plot hash.values, :filename => "#{filename}-clust.png"
-hash.values.each do |k, c_k|
-  puts "#{k}\t#{c_k}"
+def clustering_coefficients(pairs)
+  return node_clustering_coefficients(pairs).values.sort_by { |k, c_k| k }
 end
-
-=begin
-def plot_clustering_coefficient(filename, relations)
-  pairs = read_rsf_pairs(filename, relations)
-  
-  degrees = compute_node_degrees(pairs)
-  
-  data = []
-  degrees.values.each { |p| data << p }
-  
-  
-  #File.open('bli.graph', 'w') { |f| data.each { |xy| f.puts xy.join("\t") } }
-  data = {' ' => data}
-	plot data, :type => :ScatterPlot,
-			:title => "#{filename} - #{relations.join(', ')}",
-			:filename => "#{filename}-outXin-#{relations.join(',')}.png",
-      :labelx => 'out degree', :labely => 'in degree', :legend => false
-end
-
-
-if __FILE__ == $0
-  if ARGV.size < 1
-    puts "Usage: #{$0} filename.rsf [relation1 relation2 relation3 ...]"
-    exit 1
-  end
-  plot_in_out(ARGV[0], ARGV[1..-1])
-end
-=end
