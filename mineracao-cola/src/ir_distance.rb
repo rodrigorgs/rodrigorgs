0f28e06b5fcf160fc72901be3078ea05a986c6b1
rodrigorgs <rodrigorgs@dd92322b-a053-0410-994d-05c5753ab110>
2008-12-11 12:33:46 +0000

    Simplificacao dos scores da comparacao baseada em n-grams.

--- mineracao-cola/src/ir_distance.rb
+++ mineracao-cola/src/ir_distance.rb
@@ -6,33 +6,35 @@ $KCODE = 'UTF-8'
 
 require 'set'
 require 'stemmer-pt-br-rslp.rb'
-
+#require 'edit_distance'
+#require 'rgl/
 
 def assert
-	raise "Assertion failed !" unless yield # if $DEBUG
+  raise "Assertion failed !" unless yield # if $DEBUG
 end
 
-
 class DocSpace
-	attr_reader :doc_list, :terms, :idf, :weight
+  attr_reader :doc_list, :terms, :idf, :weight
+  attr_reader :edges
 
-	def initialize
-		@doc_list = Hash.new
-	end
-	
-	def add_doc(doc)
-		@doc_list[doc.name] = doc
-		return doc
-	end
+  def initialize
+    @doc_list = Hash.new
+    @edges = []
+  end
+  
+  def add_doc(doc)
+    @doc_list[doc.name] = doc
+    return doc
+  end
 
-	def done
-		@terms = []
-		@doc_list.values.each { |doc| @terms += doc.terms }
-		@terms.uniq!
+  def done
+    @terms = []
+    @doc_list.values.each { |doc| @terms += doc.terms }
+    @terms.uniq!
 
-		compute_idf
-		compute_weights
-	end
+    compute_idf
+    compute_weights
+  end
 
   # Detectando cola
   #
@@ -46,75 +48,79 @@ class DocSpace
   # TODO: normalizar valor
   # TODO: guardar coeficiente entre dois docs a fim de montar um grafo
   def coeficiente_de_colagem(doc, debug=false)
-    base = 1.5
+    other_docs = @doc_list.values - [doc]
+    edges = other_docs.map { |d| [d, 0.0] }
 
-    others = @doc_list.values - [doc]
-
-    max = 0.0
-    doc.terms.each { |term| max += (1 + base**term.count(" ")) }
+    max = doc.terms.size
 
     coef = 0.0
     doc.terms.each do |term|
-      common_docs = others.select { |other| other.terms.include? term }
-      freq = common_docs.size #/ others.size.to_f
+      common_docs = other_docs.select { |other_doc| other_doc.terms.include? term }
+      freq = common_docs.size #/ other_docs.size.to_f
       if freq > 0
-        score = (1.0 / freq) * (1 + base**term.count(" "))
-        puts "  #{'%8.2f' % score} -- #{term} -- #{common_docs.map{|x| x.name}}" if debug
+        score = (1.0 - (freq.to_f - 1) / (other_docs.size - 1)) / max.to_f
+        common_docs.each { |common_doc| edges.assoc(common_doc)[1] += score }
         coef += score
       end
     end
 
+    relevant_edges = edges.select{ |document, score| score >= 0.2 }.
+        map { |document, score| [doc, document] }
+    @edges += relevant_edges unless relevant_edges.empty?
+
+    #p relevant_edges.map{ |a, b| [a.name, b.name]} unless relevant_edges.empty?
+        #puts "  #{'%8.2f' % score} -- #{term} -- #{common_docs.map{|x| x.name}}" if debug
     #puts "max: %s,  coef: %s" % [max, coef]
-    max = 1.0 if max == 0.0
-    return coef / max
+    #max = 1.0 if max == 0.0
+    return coef #/ max
   end
 
-	def compute_idf
-		@idf = Hash.new
-		@terms.each do |term|
-			c = @doc_list.values.select { |d| d.contain? term }.size
-			if c == 0 then 
-				@idf[term] = 0  # TODO: does it make sense in IR?
-			else 
-				@idf[term] = Math.log(@doc_list.size / (0.0 + c))
-			end
-		end
-	end
+  def compute_idf
+    @idf = Hash.new
+    @terms.each do |term|
+      c = @doc_list.values.select { |d| d.contain? term }.size
+      if c == 0 then 
+        @idf[term] = 0  # TODO: does it make sense in IR?
+      else 
+        @idf[term] = Math.log(@doc_list.size / (0.0 + c))
+      end
+    end
+  end
 
-	# TODO: compute weights also for queries
-	def compute_weights
-		@weight = Hash.new
-		
-		@terms.each do |term|
-			@doc_list.values.each do |doc|
-				value = doc.freq_rel(term) * @idf[term]
-				@weight[[term,doc]] = value
+  # TODO: compute weights also for queries
+  def compute_weights
+    @weight = Hash.new
+    
+    @terms.each do |term|
+      @doc_list.values.each do |doc|
+        value = doc.freq_rel(term) * @idf[term]
+        @weight[[term,doc]] = value
 
-				assert { value >= 0 }
-			end
-		end
-	end
+        assert { value >= 0 }
+      end
+    end
+  end
 
-	def weight(keyval)
-		@weight[keyval] || 0.0
-	end
+  def weight(keyval)
+    @weight[keyval] || 0.0
+  end
 
-	def dist(doc, query)
-		num = 0
-		den1 = 0
-		den2 = 0
-		@terms.each do |term|
-			num += weight([term,doc]) * weight([term,query])
-			den1 += weight([term,doc])**2
-			den2 += weight([term,query])**2
-		end
-		den = Math.sqrt(den1) * Math.sqrt(den2)
-		if den == 0
-			return num # TODO: does it make sense in IR?
-		else
-			return (0.0 + num) / den
-		end
-	end
+  def dist(doc, query)
+    num = 0
+    den1 = 0
+    den2 = 0
+    @terms.each do |term|
+      num += weight([term,doc]) * weight([term,query])
+      den1 += weight([term,doc])**2
+      den2 += weight([term,query])**2
+    end
+    den = Math.sqrt(den1) * Math.sqrt(den2)
+    if den == 0
+      return num # TODO: does it make sense in IR?
+    else
+      return (0.0 + num) / den
+    end
+  end
 end
 
 STOPWORDS = IO.readlines('portuguese.stop').map { |x| x.strip }
@@ -140,31 +146,31 @@ def remove_acentos(string)
 end
 
 class Document
-	#attr_reader :freq_rel
-	attr_reader :name, :text
+  #attr_reader :freq_rel
+  attr_reader :name, :text
   attr_reader :terms
 
-	def freq_rel(term)
-		@freq_rel[term] || 0
-	end
+  def freq_rel(term)
+    @freq_rel[term] || 0
+  end
 
-	def initialize(name, text)
-		@name = name
-		@text = text
-		@freq_abs = Document::compute_term_freq(text)
-	
-		max_freq = @freq_abs.values.max
-		@freq_rel = Hash.new
-		@freq_abs.each_pair { |k, v| @freq_rel[k] = (0.0 + v) / max_freq }
-	end
+  def initialize(name, text)
+    @name = name
+    @text = text
+    @freq_abs = Document::compute_term_freq(text)
+  
+    max_freq = @freq_abs.values.max
+    @freq_rel = Hash.new
+    @freq_abs.each_pair { |k, v| @freq_rel[k] = (0.0 + v) / max_freq }
+  end
 
-	def contain?(term)
-		@freq_abs.has_key? term
-	end
+  def contain?(term)
+    @freq_abs.has_key? term
+  end
 
-	def self.compute_term_freq(text)
-		freq = Hash.new(0)
-		words = text.split(/[^A-Za-záéíóúàâêôãõçÁÉÍÓÚÀÂÊÔÃÕÇ]/).
+  def self.compute_term_freq(text)
+    freq = Hash.new(0)
+    words = text.split(/[^A-Za-záéíóúàâêôãõçÁÉÍÓÚÀÂÊÔÃÕÇ]/).
         select { |x| !x.empty? }
 
     words.map! { |x| remove_acentos(x) }
@@ -177,40 +183,39 @@ class Document
     words.map! { |x| stemmer.stem(x) }
 
     grams = []
-    3.upto(7) do |i|
+    2.upto(7) do |i|
       words.each_cons(i) { |x| grams << x.join(" ") }
     end
 
     words = grams # XXX  XXX   XXX     XXX      XXX
 
-		words.each do |w| freq[w] += 1 end
+    words.each do |w| freq[w] += 1 end
 
     @terms = words
-		return freq
-	end
+    return freq
+  end
 
-	def terms
-		@freq_abs.keys
-	end
+  def terms
+    @freq_abs.keys
+  end
 end
 
 def create_space()
-	space = DocSpace.new
-
-  Dir.glob("../corpora/06*/*") do |file|
-    space.add_doc(Document.new(File.basename(file), IO.read(file)))
-  end
-
-	# space.done
-	return space
 end
 
 if __FILE__ == $0
-	space = create_space
+  QUESTAO = "6.1"
+
+  space = create_space
+  space = DocSpace.new
+  Dir.glob("../corpora/questoes/#{QUESTAO}/*") do |file|
+    space.add_doc(Document.new(File.basename(file.split("-")[0]), IO.read(file)))
+  end
+
   distances = []
 
-	docs = space.doc_list.values
-	n = docs.size
+  docs = space.doc_list.values
+  n = docs.size
 
   (0..n-1).each do |i|
     distances << [docs[i], docs[i], space.coeficiente_de_colagem(docs[i])]
@@ -219,12 +224,22 @@ if __FILE__ == $0
   distances = distances.sort_by { |d1, d2, dist| dist }
 
   distances.each do |d1, d2, dist|
-    puts "%.2f %s %s" % [dist, File.basename(d1.name), File.basename(d2.name)]
+    puts "%.2f     %s" % [dist, File.basename(d1.name).split("-")[0]]
   end
 
-  puts 
-  doc = distances[-1][0]
-  #space = create_space
-  #doc = space.doc_list['AntonioRicardo-roteiro2-questao2.txt']
-  space.coeficiente_de_colagem(doc, true)
+  puts "(QUESTAO #{QUESTAO})"
+
+  require 'rgl/adjacency'
+  require 'rgl/dot'
+  edges = space.edges.map { |a, b| [a.name, b.name] }
+  p edges
+  g = RGL::DirectedAdjacencyGraph[*edges.flatten]
+  #space.doc_list.each_key { |name| g.add_vertex name }
+  g.write_to_graphic_file('png')
+  `eog graph.png`
+
+
+
+  #doc = distances[-1][0]
+  #space.coeficiente_de_colagem(doc, true)
 end
